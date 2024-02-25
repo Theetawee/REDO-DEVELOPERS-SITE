786bbf9569ea1bd13483b4ffd31f608eb40b2d6d
Forever Inc dev <redodevs@gmail.com>
2024-02-25 17:41:10 +0300

    Update WhatWedo.tsx

--- src/components/Partials/Home/WhatWedo.tsx
+++ src/components/Partials/Home/WhatWedo.tsx
@@ -1,46 +1,27 @@
-import Web from "../../../assets/web.png";
-import App from "../../../assets/app.png";
-import Cons from "../../../assets/cons.png";
-import Card from "./Card";
 const WhatWedo = () => {
     return (
-        <div className="px-4 py-8">
-            <h2 className="text-3xl font-medium text-gray-800 text-center mb-6">
+        <div>
+            <h2 className="text-3xl font-medium text-gray-800 text-center mb-3">
                 What We Do...
             </h2>
 
-            <p className="text-lg text-gray-700 mb-8 text-center">
-                We specialize in developing software solutions for individuals,
-                startups, and enterprises. Whether you're looking for a simple
-                web app, a mobile application, or enterprise-level software,
-                we're your go-to partner for all your software needs. Our
-                products are built to scale and efficiency, ensuring that your
-                business stays ahead in today's competitive landscape.
-            </p>
-
-            <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-3 gap-6">
-                <Card
-                    label="Web Development"
-                    icon={Web}
-                    description="We specialize in creating custom, responsive websites
-                        tailored to your needs. From simple landing pages to
-                        complex web applications, we've got you covered."
-                />
-                <Card
-                    label="App Development"
-                    icon={App}
-                    description="Our team builds native and cross-platform mobile
-                        applications for iOS and Android platforms. Let us turn
-                        your app ideas into reality."
-                />
-                <Card
-                    label="Consulting Services"
-                    icon={Cons}
-                    description="Need expert advice on technology strategy, architecture,
-                        or project management? Our consultants are here to help
-                        you navigate the ever-changing landscape of technology.
-                    "
-                />
+            <div className="px-4 py-8 flex-wrap md:flex-nowrap flex items-center justify-center">
+                <div>
+                    <p className="text-lg text-gray-700 mb-8">
+                        We specialize in developing software solutions for
+                        individuals, startups, and enterprises. Whether you're
+                        looking for a simple web app, a mobile application, or
+                        enterprise-level software, we're your go-to partner for
+                        all your software needs. Our products are built to scale
+                        and efficiency, ensuring that your business stays ahead
+                        in today's competitive landscape.
+                    </p>
+                </div>
+                <div className="w-full md:ml-4 flex items-center justify-center">
+                    <button className="bg-gray-900 hover:bg-gray-900/90 font-medium text-lg px-6 py-2.5 text-white">
+                        Expolre&nbsp;services
+                    </button>
+                </div>
             </div>
         </div>
     );
