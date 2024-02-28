83cde8e39b3d7c87a2efcb9bfb05b9b8e61811d1
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-02-28 14:09:48 +0300

    save

--- src/components/Partials/AppBar/Footer.tsx
+++ src/components/Partials/AppBar/Footer.tsx
@@ -1,26 +1,97 @@
-import FooterLink from "./FooterLink"
+import Logo from "../../../assets/logo.svg";
+import {Link} from "react-router-dom";
 
 const Footer = () => {
-  return (<>
-      <footer className="bg-white p-4 flex items-center justify-center flex-col">
-          <div className="w-full max-w-screen-md mx-auto mb-6 md:grid-cols-3 grid grid-cols-2 gap-4">
-              <FooterLink label="Home" path="/" />
-              <FooterLink label="Carrers" path="/carrers" />
-              <FooterLink label="Contact Us" path="/contact-us" />
-              <FooterLink label="About Us" path="/about-us" />
-              <FooterLink label="FAQ" path="/faq" />
-              <FooterLink label="Terms of Service" path="/terms-of-service" />
-              <FooterLink label="Blog" path="/blog" />
-              <FooterLink label="Privacy Policy" path="/privacy-policy" />
-          </div>
-          <div>
-              <p className="text-gray-600 text-center font-medium">
-                  Copyright© 2024, All rights reserved{" "}
-                  <span className="text-primary-500">Redo Developers Inc.</span>
-              </p>
-          </div>
-      </footer></>
-  );
-}
+    return (
+        <footer className="bg-gray-100 border-t border-gray-300 text-gray-700">
+            <div className="container mx-auto py-10 md:py-16 lg:py-20 px-4">
+                <div className="grid grid-cols-1 md:grid-cols-2 lg:grid-cols-4 gap-8">
+                    <div>
+                        <h5 className="text-lg font-medium mb-2">Company</h5>
+                        <ul className="text-gray-700">
+                            <li className="mb-3">
+                                <Link
+                                    to="/about"
+                                    className="hover:text-primary-500 text-sm"
+                                >
+                                    About Us
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5 className="text-lg font-medium mb-2">
+                            Help Center
+                        </h5>
+                        <ul className="text-gray-700">
+                            <li className="mb-3">
+                                <Link
+                                    to="/contact"
+                                    className="hover:text-primary-500 text-sm"
+                                >
+                                    Contact Us
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5 className="text-lg font-medium mb-2">Legal</h5>
+                        <ul className="text-gray-700">
+                            <li className="mb-3">
+                                <Link
+                                    to="#"
+                                    className="hover:text-primary-500 text-sm"
+                                >
+                                    Privacy Policy
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                    <div>
+                        <h5 className="text-lg font-medium mb-2">Relations</h5>
+                        <ul className="text-gray-700">
+                            <p>Profiles</p>
 
-export default Footer
+                            <li className="mb-3">
+                                <Link
+                                    to="/profiles/founder"
+                                    className="hover:text-primary-500 text-sm"
+                                >
+                                    Founder
+                                </Link>
+                            </li>
+                            <li className="mb-3">
+                                <Link
+                                    to="/meeting"
+                                    className="hover:text-primary-500 text-sm"
+                                >
+                                    Demo
+                                </Link>
+                            </li>
+                        </ul>
+                    </div>
+                </div>
+                <hr className="my-8 border-gray-400" />
+                <div className="text-center">
+                    <Link
+                        to="/about"
+                        className="flex items-center justify-center mb-5 text-xl font-medium"
+                    >
+                        <img
+                            src={Logo}
+                            className="w-8 h-8 mr-2"
+                            alt="Redo Developers Inc. logo"
+                        />
+                        Redo Developers Inc.
+                    </Link>
+                    <p className="text-sm text-gray-700">
+                        &copy; 2021-2023 Redo Developers Inc. All Rights
+                        Reserved.
+                    </p>
+                </div>
+            </div>
+        </footer>
+    );
+};
+
+export default Footer;
--- src/components/Partials/Home/Testmonies.tsx
+++ src/components/Partials/Home/Testmonies.tsx
@@ -1,47 +1,59 @@
-import { MDBCarousel, MDBCarouselItem } from 'mdb-react-ui-kit';
-import "mdb-react-ui-kit/dist/css/mdb.min.css";
-import Card from './Card';
+import { Swiper, SwiperSlide } from "swiper/react";
+import "swiper/css";
+import Card from "./Card";
 import Customer1 from "../../../assets/cloud.jpg";
 import Customer2 from "../../../assets/cust.jpg";
 
-
 export default function Testmonies() {
-  return (<div>
-      <MDBCarousel  touch={true}  interval={5000}>
-          <MDBCarouselItem itemId={1}>
-              <Card
-                  image={Customer1}
-                  stars={2}
-                  description={
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                  }
-                  name='Vannessa'
-                  title='CEO'
-              />
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId={2}>
-              <Card
-                  image={Customer2}
-                  stars={4}
-                  name='Jane de Cruz'
-                  title="Chairman"
 
-                  description={
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                  }
-              />
-          </MDBCarouselItem>
-          <MDBCarouselItem itemId={3}>
-              <Card
-                  image={Customer1}
-                  stars={5}
-                  name="James"
-                  title='CFO'
-                  description={
-                      "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
-                  }
-              />
-          </MDBCarouselItem>
-      </MDBCarousel></div>
-  );
+
+    return (
+        <>
+            <Swiper
+                spaceBetween={50}
+                slidesPerView={1}
+                autoplay={true}
+                loop={true}
+
+
+
+                // onSlideChange={() => console.log("slide change")}
+                // onSwiper={(swiper) => console.log(swiper)}
+            >
+                <SwiperSlide>
+                    <Card
+                        image={Customer1}
+                        stars={2}
+                        description={
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        }
+                        name="Vannessa"
+                        title="CEO"
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Card
+                        image={Customer2}
+                        stars={4}
+                        name="Jane de Cruz"
+                        title="Chairman"
+                        description={
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        }
+                    />
+                </SwiperSlide>
+                <SwiperSlide>
+                    <Card
+                        image={Customer1}
+                        stars={5}
+                        name="James"
+                        title="CFO"
+                        description={
+                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                        }
+                    />
+                </SwiperSlide>
+            </Swiper>
+        </>
+    );
 }
