79dc8c4a1a5ce92561bc18ff92dbf6470e4987db
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-02-29 15:45:11 +0300

    save

--- src/components/Partials/Home/Card.tsx
+++ src/components/Partials/Home/Card.tsx
@@ -20,17 +20,22 @@ const TestimonialCard = ({
                     <div className="flex items-center mb-4">
                         <img
                             src={image}
-                            className="w-20 h-20 rounded-full mr-4"
+                            className="w-14 h-14 rounded-full mr-4"
                             alt={name}
                         />
                         <div>
-                            <p className="text-xl uppercase mb-0 font-medium text-gray-700">
+                            <p className="text-xl mb-0 font-medium text-gray-700">
                                 {name}
                             </p>
-                            <p className="text-gray-600 leading-3 italic text-sm">{title}</p>
+                            <p className="text-gray-600 leading-3 italic text-sm">&#8212; {title}</p>
                         </div>
-                    </div>
-                    <p className="text-gray-700 mb-4">{description}</p>
+                    </div><span>
+                        <svg className="w-6 h-6 text-primary-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M0 216C0 149.7 53.7 96 120 96h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H64c-35.3 0-64-28.7-64-64V320 288 216zm256 0c0-66.3 53.7-120 120-120h8c17.7 0 32 14.3 32 32s-14.3 32-32 32h-8c-30.9 0-56 25.1-56 56v8h64c35.3 0 64 28.7 64 64v64c0 35.3-28.7 64-64 64H320c-35.3 0-64-28.7-64-64V320 288 216z"/></svg>
+                    </span>
+                    <p className="text-gray-700  pl-8">{description}</p>
+                    <span className="flex mb-6 items-center justify-end">
+                        <svg className="w-6 h-6 text-primary-500" fill="currentColor" xmlns="http://www.w3.org/2000/svg" viewBox="0 0 448 512"><path d="M448 296c0 66.3-53.7 120-120 120h-8c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H320c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72zm-256 0c0 66.3-53.7 120-120 120H64c-17.7 0-32-14.3-32-32s14.3-32 32-32h8c30.9 0 56-25.1 56-56v-8H64c-35.3 0-64-28.7-64-64V160c0-35.3 28.7-64 64-64h64c35.3 0 64 28.7 64 64v32 32 72z"/></svg>
+                    </span>
                     <div className="border-t border-gray-200 pt-4">
                         <div className="flex items-center justify-center">
                             {/* display the stars equivalent to the number */}
@@ -39,7 +44,8 @@ const TestimonialCard = ({
                             ))}
                         </div>
                     </div>
-                </div>
+
+                </div><div className="items-center flex text-sm text-primary-600 justify-center"><p>Swipe to read more</p></div>
             </div>
         </div>
     );
--- src/components/Partials/Home/Testmonies.tsx
+++ src/components/Partials/Home/Testmonies.tsx
@@ -1,10 +1,10 @@
 import { Swiper, SwiperSlide } from "swiper/react";
-
+import User from "../../../assets/twitter-avi-gender-balanced-figure.png";
 import "swiper/css";
 import Card from "./Card";
-import Customer1 from "../../../assets/cloud.jpg";
-import Customer2 from "../../../assets/cust.jpg";
-import { Autoplay } from "swiper/modules";
+// import Customer1 from "../../../assets/cloud.jpg";
+// import Customer2 from "../../../assets/cust.jpg";
+// import { Autoplay } from "swiper/modules";
 
 export default function Testmonies() {
     return (
@@ -14,44 +14,44 @@ export default function Testmonies() {
                 slidesPerView={1}
                 autoplay={{
                     delay: 2500,
-                    disableOnInteraction: false,
+                    disableOnInteraction: true,
                 }}
                 loop={true}
-                modules={[Autoplay]}
+                // modules={[Autoplay]}
                 pagination={{
                     clickable: true,
                 }}
             >
                 <SwiperSlide>
                     <Card
-                        image={Customer1}
-                        stars={2}
+                        image={User}
+                        stars={5}
                         description={
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                            "I can't say enough good things about Redo Developers Inc.! Their custom software solutions have revolutionized the way we do business. From the get-go, they understood our needs and delivered a product that exceeded our expectations. Their team is not just professional but genuinely caring about our success. Highly recommend them!"
                         }
-                        name="Vannessa"
-                        title="CEO"
+                        name="Glen Meakem"
+                        title="CEO and Founder of Forever Inc."
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <Card
-                        image={Customer2}
-                        stars={4}
-                        name="Jane de Cruz"
-                        title="Chairman"
+                        image={User}
+                        stars={5}
+                        name="Jane"
+                        title="Business Woman"
                         description={
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                            "Working with Redo Developers Inc. has been an absolute pleasure. They didn't just build us a website; they crafted an online experience that resonates with our audience. Their attention to detail and commitment to delivering top-notch results are unparalleled. If you're looking for a web development partner you can trust, look no further"
                         }
                     />
                 </SwiperSlide>
                 <SwiperSlide>
                     <Card
-                        image={Customer1}
+                        image={User}
                         stars={5}
                         name="James"
-                        title="CFO"
+                        title="Business Owner"
                         description={
-                            "Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua."
+                            "Partnering with Redo Developers Inc. for software consulting was one of the best decisions we've made. Their consultants are not only experts in their field but also fantastic listeners. They took the time to understand our unique challenges and provided tailored solutions that fit our needs perfectly. Couldn't be happier with the outcome!"
                         }
                     />
                 </SwiperSlide>
