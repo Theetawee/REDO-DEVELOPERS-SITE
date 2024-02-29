8cb8e1f5841d20fb2bd4f9e71122267ef7333db2
Khaotungkulmethee Pattawee <140291119+Theetawee@users.noreply.github.com>
2024-02-29 13:31:49 +0300

    Update AppBarLink.tsx

--- src/components/Partials/AppBar/AppBarLink.tsx
+++ src/components/Partials/AppBar/AppBarLink.tsx
@@ -3,9 +3,14 @@ import { NavLink } from "react-router-dom"
 import { DrawerContext } from "../../../context/DrawerContext"
 
 const AppBarLink = ({ label, path }: { label: string, path: string }) => {
-  const {toggleSidebar } = useContext(DrawerContext);
+  const { toggleSidebar,isOpen } = useContext(DrawerContext);
+  const handleOnclick = () => {
+    if (isOpen) {
+      toggleSidebar()
+    }
+  }
   return (
-    <NavLink title={label} onClick={toggleSidebar} className={"appbar-link"} to={path}>{label}</NavLink>
+    <NavLink title={label} onClick={handleOnclick} className={"appbar-link"} to={path}>{label}</NavLink>
   )
 }
 
